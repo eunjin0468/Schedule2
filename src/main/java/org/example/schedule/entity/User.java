b181b0a611d61d6ff7146ebb1ff861d771ebff3f
dxxwls <eunjin0468@naver.com>
2025-08-13 00:41:21 +0900

    feat: User 엔티티에 password 필드 추가

--- src/main/java/org/example/schedule/entity/User.java
+++ src/main/java/org/example/schedule/entity/User.java
@@ -3,9 +3,14 @@ package org.example.schedule.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "users")
 public class User extends BaseEntity {
@@ -14,16 +19,28 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    @Column(nullable = false, unique = true)
     private String userName;
     private String email;
 
+    @Column(nullable = false)
+    private String password;
+
+    //양방향일경우
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private final List<Schedule> schedules = new ArrayList<>();
+
     public User(
             String userName,
-            String email
+            String email,
+            String password
     ){
         this.userName = userName;
         this.email = email;
+        this.password = password;
     }
+
+
 
     //update method
     public void updateUser(String userName, String email){
